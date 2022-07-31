1fb5eab4a68e69cb3198367993346da0a0250130
sivasankarchinnasamy <siva.vnit.1986@gmail.com>
2022-07-31 21:26:45 +0530

    08 file update

---
+++ 08.Variables.py
@@ -0,0 +1,18 @@
+print("Hello World")
+'''Type Casting Examples
+int()
+float()
+string()
+'''
+var1 = '30'
+var2 = '50'
+print(var1+var2)
+print(int(var1)+int(var2))
+
+'''
+input() - arguments always come as string
+'''
+print("Enter any number: ")
+ipnum = input()
+print("Entered number + 10 is: "+ str(int(ipnum)+10)) # concat only works for string
+print("Entered number + 10 is: ", int(ipnum)+10) # int passed as argument
