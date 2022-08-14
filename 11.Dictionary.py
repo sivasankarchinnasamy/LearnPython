c39c098b5ab4fb991e37a66d6098fdc483656ab4
sivasankarchinnasamy <siva.vnit.1986@gmail.com>
2022-08-14 22:44:55 +0530

    11 file updated

--- 11.Dictionary.py
+++ 11.Dictionary.py
@@ -14,6 +14,23 @@ dict["Address"] = {"Street":"Anup Reddy Layout","Pincode":560048,"City":"Bangalo
 print(dict["Address"]["Street"])
 print(dict["Siva"].split(" ")[2])
 
+# Access using get() function
+print(dict.get("Siva"))
+
 # Delete using key
 del dict["Phone"]
 print(dict)
+
+# Update data using update()
+dict.update({"Phone":9503181980})
+print(dict)
+dict.update({"Phone":8904323564}) #updated phone number
+print(dict)
+
+# get Keys
+list = dict.keys()
+print(list) # gives tuple of keys
+
+# get items
+items = dict.items()
+print(items) # gives tuple of items
